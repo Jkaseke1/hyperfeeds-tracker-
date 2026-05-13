7e03e94bb24b6c7939ec796c37e2ec4190b2bae2
Jkaseke1 <jkaseke1@users.noreply.github.com>
2026-05-13 12:15:29 +0200

    Bump storage v6 -> v7 to clear cache and load new IT SOP task

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Comments from './components/Comments.jsx'
 import Inbox from './components/Inbox.jsx'
 
 // ---------- Persistence ----------
-const STORAGE_KEY = 'hyperfeeds-tracker:v6'
+const STORAGE_KEY = 'hyperfeeds-tracker:v7'
 
 function loadInitial() {
   try {
